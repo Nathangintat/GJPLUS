29bf70543a6c9ca0d3541dfd3c87369115e4c8d5
Shinichi Wijaya <Lightsoul1080@gmail.com>
2024-11-03 21:58:45 +0700

    Add SFX

--- Assets/2D/Script/GameSystem2D.cs
+++ Assets/2D/Script/GameSystem2D.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class GameSystem2D : MonoBehaviour
 {
+    [SerializeField] private AudioClip _screamSFX;
+    [SerializeField] private AudioClip _monsterSFX;
+    [SerializeField] private AudioClip _altMonsterSFX;
     public static GameSystem2D Instance;
-    public int deathCount = 0;
+    private AudioSource _audioSource;
     private void Awake() {
         if(Instance == null)
         {
@@ -15,5 +19,21 @@ public class GameSystem2D : MonoBehaviour
         else {
             Destroy(this);
         }
+    }
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+    public void MonsterSFX()
+    {
+        _audioSource.PlayOneShot(_monsterSFX);
+    }
+    public void AltMonsterSFX()
+    {
+        _audioSource.PlayOneShot(_altMonsterSFX);
+    }
+    public void ScreamSFX()
+    {
+        _audioSource.PlayOneShot(_screamSFX);
     }
 }
