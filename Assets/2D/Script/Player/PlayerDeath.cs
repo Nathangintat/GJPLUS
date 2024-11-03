3301cadf499216d7fc21a4cb2826059df2ce0d2f
Shinichi Wijaya <Lightsoul1080@gmail.com>
2024-11-03 19:53:34 +0700

    Update :fire:

--- Assets/2D/Script/Player/PlayerDeath.cs
+++ Assets/2D/Script/Player/PlayerDeath.cs
@@ -1,20 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    [SerializeField] private readonly int lives = 3;
-    
+    [SerializeField] private GameObject DeathScreen;
     public void Die(){
-        if(GameSystem2D.Instance.deathCount >= lives) {
-            FinalDeath();
-            return;
-        }
-        GameSystem2D.Instance.deathCount++;
-        Debug.Log("Die");
-    }
-    public void FinalDeath(){
-        Debug.Log("Final Death");
+        DeathScreen.SetActive(true);
+        gameObject.SetActive(false);
+        SceneManager.LoadScene(1);
     }
 }
